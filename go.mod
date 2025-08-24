9185a1fa630d6dde0392cf6bc3fc02358b17755f
Antoni Marzec <amarzec@outlook.com>
2025-08-24 10:58:32 +0200

    move to semver

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/marzeq/mconf
+module github.com/marzeq/mconf/v2
 
 go 1.22.5
--- mconf.go
+++ mconf.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-  VERSION  = "1.2508.1"
+  VERSION  = "2.0.0"
   PROGNAME = "mconf"
 )
 
